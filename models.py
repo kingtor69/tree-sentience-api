d90c3e6a067ee6256590a548aec7304d65350196
Tor Kingdon <tor@hearkitty.com>
2022-01-16 14:33:38 -0700

    changes in vscode buffer?

--- models.py
+++ models.py
@@ -1,18 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-import bcrypt
+from flask_bcrypt import Bcrypt
 from datetime import datetime
 import json
-from helpers import *
-from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+bcrypt = Bcrypt()
 
 def connect_db(app):
     db.app = app
     db.init_app(app)
 
+
 class User(db.Model):
-    """User model. For end users, this is transparent and only used to keep track of which email address is associated with which room. Their passwords are created in app.py and are unimportant as they don't ever need to log in. For creators and admins, an actual password should be entered. 
+    """User model. user.id is used in Route and Checkpoint models as those will both be stored within the user. 
     """
 
     __tablename__ = "users"
@@ -27,34 +28,38 @@ class User(db.Model):
             self.privileges_logical = "end_user"
 
         return {
-            "email": self.email,
+            "id": self.id,
             "name": self.name,
-            "privileges": self.privileges_logical
+            "privileges": self.privileges
         }
     
-    email = db.Column(db.String,
-                      primary_key=True,
-                      nullable=False,
-                      unique=True)
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
     name = db.Column(db.String, 
                      nullable=False)
+    email = db.Column(db.String,
+                      nullable=False,
+                      unique=True)
     privileges = db.Column(db.Integer,
-                           nullable=False)
+                      nullable=False)
     password = db.Column(db.String,
                          nullable=False)
     
+    room = db.relationship("RoomData", cascade="all, delete", backref="user")
+
     def __repr__(self):
-        return f'<User {self.email}: {self.privileges_logical}>'
+        return f'User#{self.id}: {self.username} {self.email} {self.privileges_logical}'
 
     @classmethod
-    def hashpass(cls, email, password, privileges):
-        """Generate new user with email, password and privileges.
+    def hashpass(cls, username, password):
+        """Generate new user with username and hashed password only, other fields still to be populated.
         """
 
         hashed = bcrypt.generate_password_hash(password)
         hashed_utf8 = hashed.decode("utf8")
 
-        return cls(email=email, password=hashed_utf8, privileges=privileges)
+        return cls(username=username, password=hashed_utf8)
 
     @classmethod
     def authenticate(cls, username, password):
@@ -67,14 +72,6 @@ class User(db.Model):
             return user
 
         return False
-
-ROOM_REQUIRED = ['template', 'color', 'message']
-ROOM_OPTIONAL = ['recipient', 'animal']
-ROOM_PRIMARY = 'id'
-ROOM_FOREIGN = 'user_id'
-
-ROOM_KEYS = [ ROOM_PRIMARY, ROOM_FOREIGN ]
-ROOM_FIELDS = concatinate_lists( [ ROOM_REQUIRED, ROOM_OPTIONAL ] )
 
 class RoomData(db.Model):
     """Model for room data. This data is originally provided by end users and accessible to creators and admins. Rooms are linked to user_id of the end user.
@@ -97,22 +94,70 @@ class RoomData(db.Model):
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
-    template = db.Column(db.String(20),
-                         nullable=False)  
-    color = db.Column(db.String(20),
-                      nullable=False)
+    template = db.Column(db.String(16),
+                         nullabe=False)  
+    timestamp = db.Column(db.DateTime,
+                          default=datetime.utcnow())
     user_id = db.Column(db.Integer,
                         db.ForeignKey("users.id"))
+    color = db.Column(db.String(16),
+                      nullable=False)
     message = db.Column(db.String(256),
+)
+
+    checkpoint_route = db.relationship("CheckpointRoute", cascade="all, delete", backref="route_cpr")
+
+class Checkpoint(db.Model):
+    """Checkpoint model for intermediate geocoded points used as either stopping places or to alter route. 
+    """
+
+    __tablename__ = "checkpoints"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "checkpoint_display_name": self.checkpoint_display_name,
+            "checkpoint_lat": self.checkpoint_lat,
+            "checkpoint_lng": self.checkpoint_lng
+        }
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey("users.id"))
+    checkpoint_display_name = db.Column(db.String)
+    checkpoint_lat = db.Column(db.Float,
                         nullable=False)
-    recipient = db.Column(db.String)
-    animal = db.Column(db.String)
+    checkpoint_lng = db.Column(db.Float,
+                        nullable=False)
 
-    user = db.relationship("User", cascade="all, delete")
+    checkpoint_route = db.relationship("CheckpointRoute", backref="checkpoint")
 
-    def __repr__(self):
-        room_repr = f"<Room Data {self.id}: \n"
-        room_repr += f"from {self.user_id}, "
-        for field in ROOM_FIELDS:
-            room_repr += f"{field}={self.field}, "
-        return room_repr.strip(', ') + ">"
+
+class CheckpointRoute(db.Model):
+    """Route-checkpoint model shows in what route and in what order checkpoints are used. 
+    """
+
+    __tablename__ = "checkpoints_routes"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "route_id": self.route_id,
+            "checkpoint_id": self.checkpoint_id,
+            "route_order": self.route_order
+        }
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    route_id = db.Column(db.Integer,
+                         db.ForeignKey("routes.id"), 
+                         nullable=False)
+    checkpoint_id = db.Column(db.Integer,
+                              db.ForeignKey("checkpoints.id"),
+                              nullable=False)
+    route_order = db.Column(db.Integer,
+                            nullable=False)
